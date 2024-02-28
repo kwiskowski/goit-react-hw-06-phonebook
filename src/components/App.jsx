69f83c18aec913ed1cd9e2434fe3dfc487b3c9a4
Krzysztof Wiskowski <krzysztof.wiskowski@gmail.com>
2024-02-29 00:00:02 +0100

    redux actions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,12 +5,12 @@ import { PhonebookList } from './PhonebookList/PhonebookList';
 import { Filter } from './PhonebookFilter/PhonebookFilter';
 import { Section } from './Section/Section';
 
-const phoneContacts = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
+// const phoneContacts = [
+//   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+// ];
 
 export const App = () => {
   const [contacts, setContacts] = useState(() => {
